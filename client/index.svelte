3a4f0173f7453fa5e00edbeb12c964dd44d673cb
Josh Duff <me@JoshDuff.com>
2025-09-12 23:57:07 -0500

    I have hopefully vibe-coded my way to a working svelte build, at least for dev

---
+++ client/index.svelte
@@ -0,0 +1,7 @@
+<script>
+	let name = "world"
+</script>
+
+<h1>Hello {name}!</h1>
+<p>Welcome to Svelte</p>
+<p>hell yeah baby</p>
---
+++ esbuild.config.ts
@@ -0,0 +1,29 @@
+import { build, context } from 'esbuild'
+import sveltePlugin from 'esbuild-svelte'
+
+const is_watch = process.argv.includes('--watch')
+
+const build_options = {
+	entryPoints: ['client/index.ts'],
+	bundle: true,
+	outdir: 'public/build',
+	plugins: [sveltePlugin({
+		compilerOptions: {
+			dev: is_watch
+		}
+	})],
+	format: 'esm' as const,
+	target: 'es2020',
+	loader: {
+		'.ts': 'ts',
+	},
+}
+
+if (is_watch) {
+	const ctx = await context(build_options)
+	await ctx.watch()
+	console.log('Watching for changes...')
+} else {
+	await build(build_options)
+	console.log('Build complete')
+}
